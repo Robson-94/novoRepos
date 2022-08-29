98cd4627e970ee7cdf0e59c7ace0eaf820d5ccbd
Robson Martins <rhobinson23@gmail.com>
2022-08-29 01:26:20 -0300

    meu teste 1. Por favor, funciona!

---
+++ js/owl/setup.js
@@ -0,0 +1,25 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+
+/*function pop_up(){
+    jan=open("pagina.htm", "dd","width=500,height=400")
+}*/
+
+function center(){
+    
+    
+}
